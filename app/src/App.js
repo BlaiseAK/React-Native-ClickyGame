adf12d812c3a47fc7b9ca173e1e137f8f1ed0f3e
BlaiseAK <blaise.a.kress@gmail.com>
2018-08-27 01:12:24 -0400

    having issures with the react-native import

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {Text, View} from 'react-native';
+// import logo from './logo.svg';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello world!</Text>
+      </View>
+    );
+  }
+}
+
